8a8a51645989ca0cc2ffab31e4809dc6874386ec
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-10-03 23:45:07 -0500

    chore | Se revisó y corrigió Codigo4

--- src/com/generation/Codigo4.java
+++ src/com/generation/Codigo4.java
@@ -1,39 +1,43 @@
 package com.generation;
 
+import java.util.Scanner; //faltaba importar para función Scanner
+
 public class Codigo4 {
 
-    Scanner s = new Scanner();
+	public static void main(String[] args) { //no poseía un método main para ejecutar el código
+    Scanner s = new Scanner(System.in);//no se tenía que es lo que se iba a escanear
     
     System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
     String j1 = s.nextLine();
     
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
+    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): "); //error en el número del jugador
+    //Scanner s2 = new Scanner(System.in);//no se tenía que es lo que se iba a escanear y es código innecesario
+    String j2 = s.nextLine();//se añadió s2
+
+    if (j1 == j2) {//había un paréntesis de más
+      System.out.println("Empate");//multiples errores en el cierre de llaves
     } else {
       int g = 2;
       switch(j1) {
         case "piedra":
-          if (j2 == "tijeras") {
+          if (j2.equals("tijeras")) {//la sentencia == no estaba aplicando correctamente
             g = 1;
           }
-
+          break;//faltaba agregar un break a cada condicion de case
         case "papel":
-          if (j2 == "piedra") {
+          if (j2.equals("piedra")) {
             g = 1;
-          
-        case "tijera":
+            }
+          break;
+        case "tijeras"://faltaba una "s"
           if (j2.equals("papel")) {
             g = 1;
-          }
+          }//if papel
           break;
         default:
-      }
+      }//switch
       System.out.println("Gana el jugador " + g);
-    }
-  
-  
-}
+    }//else
+    s.close();
+   }//main
+}//class Codigo4
