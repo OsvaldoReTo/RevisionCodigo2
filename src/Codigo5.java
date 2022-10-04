8eec5c6d91b152f7cfcfc428f4e67d598705f12d
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-10-04 15:23:27 -0500

    chore | Se corrigió código 5

--- src/Codigo5.java
+++ src/Codigo5.java
@@ -1,29 +1,36 @@
-public class Codigo5 {
+import java.util.Scanner; //faltaba importar java.util.Scanner
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
+public class Codigo5 {
+	
+	public static void main (String[] args) { //falta método Main
+
+	    Scanner s = new Scanner(System.in);//no se indicó argumento para scanner
+	    System.out.print("Introduzca un número: ");//no se ocupó una doble comilla en cierre
 	    String ni = s.nextLine();
-	    int c = ni;
+	    int c = Integer.parseInt(ni);//se intenta guardar un dato tipo String en un tipo int
 	    
 	    int afo = 0;
 	    int noAfo = 0;
 	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
+	    while (c > 0) { //sustituir c en cada parte donde aparezca ni, para tomarlo como entero
+		  int digito = (int)(c % 10);
 	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
 			afo++;
 	      } else {
 			noAfo++;
 		  
-		  ni /= 10;
-	    }
+		   //error de posición de c/=10
+	    }//if-else
 
+	      c /= 10;
+		  }//while //mal colocación de while, solo para que se pinte una vez el resultado
 	    if (afo > noAfo) {
-	      System.out.prinln("El " + c + " es un número afortunado.");
+	      System.out.println("El " + ni + " es un número afortunado.");//mal escrito el método System.out.println();
 	    } else {
-	      System.out.println("El " + c + " no es un número afortunado.");
-	    }
+	      System.out.println("El " + ni + " no es un número afortunado.");
+	    }//if-else
 	    
-	  }
-	  
-	}
+
+	    s.close();//cierre de scanner
+	 }//main
+	}//class Codigo5
